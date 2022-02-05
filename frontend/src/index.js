159c8874ae2bffe5ae6a45de057dc6b1133bc887
DALGA Ozguc <ozgucdalga@gmail.com>
2022-02-04 23:41:02 -0500

    routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
+
+import Products from "./components/Products";
+import Cart from "./components/Cart";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <Routes>
+     <Route exact path="/" component={Products} />
+      <Route path="/components/cart" component={Cart} />
       <App />
+      </Routes>
     </Router>
   </React.StrictMode>,
   rootElement
