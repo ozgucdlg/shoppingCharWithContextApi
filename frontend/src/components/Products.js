0f96ff768bc4a44289c6a289c3b26e6415d4dc5f
DALGA Ozguc <ozgucdalga@gmail.com>
2022-02-08 23:23:23 -0500

    mapUpdated

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -9,7 +9,7 @@ const Products = props => {
 const context = useContext(BooksContext);
   
 
-  return (
+  return ( 
     <div>
       <h2>
         <span>Kitap Listesi</span>
@@ -17,75 +17,34 @@ const context = useContext(BooksContext);
       </h2>
 
 
-      {context.map(kitap => (
-          console.log(kitap.name)
-        )
-        )
-      }
+
+{/* 
+
+      {context.map(book => (
+
+      <div  clasName="book">
+          <img src={book.image } alt={book.name}  /> 
+       <div>
+         <h4>{book.name}</h4>
+         <p>{book.author}</p>
+         <p>fiyat : {book.price}</p>
+         <button>sepete ekle</button>
+       </div>
+
+       </div>  
+
+      ))}
+
+      
+      
+          
+      
 
 
 
+ */}
 
-
-      {/* <div className="book">
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/51eqjXwFzwL._SX344_BO1,204,203,200_.jpg"
-          alt="Simyaci"
-        />
-        <div>
-          <h4>Simyaci</h4>
-          <p>Yazar: Paulo Coelho</p>
-          <p>Fiyat: &#8378; 19.99</p>
-          <button>Sepete Ekle</button>
-        </div>
-      </div>
-
-      <div className="book">
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/51eqjXwFzwL._SX344_BO1,204,203,200_.jpg"
-          alt="Simyaci"
-        />
-        <div>
-          <h4>Simyaci</h4>
-          <p>Yazar: Paulo Coelho</p>
-          <p>Fiyat: &#8378; 19.99</p>
-          <button>Sepete Ekle</button>
-        </div>
-      </div><div className="book">
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/51eqjXwFzwL._SX344_BO1,204,203,200_.jpg"
-          alt="Simyaci"
-        />
-        <div>
-          <h4>Simyaci</h4>
-          <p>Yazar: Paulo Coelho</p>
-          <p>Fiyat: &#8378; 19.99</p>
-          <button>Sepete Ekle</button>
-        </div>
-      </div><div className="book">
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/51eqjXwFzwL._SX344_BO1,204,203,200_.jpg"
-          alt="Simyaci"
-        />
-        <div>
-          <h4>Simyaci</h4>
-          <p>Yazar: Paulo Coelho</p>
-          <p>Fiyat: &#8378; 19.99</p>
-          <button>Sepete Ekle</button>
-        </div>
-      </div><div className="book">
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/51eqjXwFzwL._SX344_BO1,204,203,200_.jpg"
-          alt="Simyaci"
-        />
-        <div>
-          <h4>Simyaci</h4>
-          <p>Yazar: Paulo Coelho</p>
-          <p>Fiyat: &#8378; 19.99</p>
-          <button>Sepete Ekle</button>
-        </div>
-      </div> */}
-
+      
 
 
 
